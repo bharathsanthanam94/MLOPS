b3b02cc460cb0b700326ebe31dd5a53d9e690b65
bharathsanthanamd1 <bharath@DESKTOP-L16E59N.localdomain>
2023-02-17 19:08:39 +0100

    added code for EDA, Feature selection

--- churn_library.py
+++ churn_library.py
@@ -1,10 +1,16 @@
 # library doc string
-
+'''
+This module predicts customer who are likely to churn based on the input features.
+'''
 
 # import libraries
 import os
-os.environ['QT_QPA_PLATFORM']='offscreen'
-
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns 
+from sklearn.model_selection import train_test_split
+sns.set()
+os.environ['QT_QPA_PLATFORM'] = 'offscreen'
 
 
 def import_data(pth):
@@ -15,11 +21,14 @@ def import_data(pth):
             pth: a path to the csv
     output:
             df: pandas dataframe
-    '''	
-	pass
+    '''
+    data_frame = pd.read_csv(pth)
+    data_frame["Churn"] = data_frame['Attrition_Flag'].apply(
+        lambda val: 0 if val == "Existing Customer" else 1)
+    return data_frame
 
 
-def perform_eda(df):
+def perform_eda(data_frame):
     '''
     perform eda on df and save figures to images folder
     input:
@@ -28,10 +37,24 @@ def perform_eda(df):
     output:
             None
     '''
-	pass
+    column_names_list = ["Churn", "Customer_Age","Marital_Status","Total_Trans_Ct","heat_map"]
+    for column in column_names_list:
+        plt.figure(figsize=(20, 10))
+        if column == "Churn":
+            data_frame['Churn'].hist()
+        elif column == "Customer_Age":
+            data_frame["Customer_Age"].hist()
+        elif column == "Marital_Status":
+            data_frame.Marital_Status.value_counts('normalize').plot(kind='bar')
+        elif column == "Total_Trans_Ct":
+            sns.histplot(data_frame['Total_Trans_Ct'], stat='density', kde=True)
+        elif column == "heat_map":
+            sns.heatmap(data_frame.corr(), annot=False, cmap='Dark2_r', linewidths = 2)
+        plt.savefig("images/eda/" + column + ".jpg")
+        plt.close()
 
 
-def encoder_helper(df, category_lst, response):
+def encoder_helper(data_frame, category_lst):
     '''
     helper function to turn each categorical column into a new column with
     propotion of churn for each category - associated with cell 15 from the notebook
@@ -39,19 +62,27 @@ def encoder_helper(df, category_lst, response):
     input:
             df: pandas dataframe
             category_lst: list of columns that contain categorical features
-            response: string of response name [optional argument that could be used for naming variables or index y column]
+            response: string of response name [optional argument that could be
+            used for naming variables or index y column]
 
     output:
             df: pandas dataframe with new columns for
     '''
-    pass
+    for cat_feature in category_lst:
+        cat_feature_churn= cat_feature+"_Churn"
+        cat_feature_list =[]
+        cat_feature_groups =data_frame.groupby(cat_feature).mean()['Churn']
+        for val in data_frame[cat_feature]:
+            cat_feature_list.append(cat_feature_groups.loc[val])
+        data_frame[cat_feature_churn]=cat_feature_list
+    return data_frame
 
-
-def perform_feature_engineering(df, response):
+def perform_feature_engineering(data_frame):
     '''
     input:
               df: pandas dataframe
-              response: string of response name [optional argument that could be used for naming variables or index y column]
+              response: string of response name [optional argument that 
+              could be used for naming variables or index y column]
 
     output:
               X_train: X training data
@@ -59,6 +90,20 @@ def perform_feature_engineering(df, response):
               y_train: y training data
               y_test: y testing data
     '''
+    y_target=data_frame["Churn"]
+    x_features=pd.DataFrame()
+    keep_cols = ['Customer_Age', 'Dependent_count', 'Months_on_book',
+             'Total_Relationship_Count', 'Months_Inactive_12_mon',
+             'Contacts_Count_12_mon', 'Credit_Limit', 'Total_Revolving_Bal',
+             'Avg_Open_To_Buy', 'Total_Amt_Chng_Q4_Q1', 'Total_Trans_Amt',
+             'Total_Trans_Ct', 'Total_Ct_Chng_Q4_Q1', 'Avg_Utilization_Ratio',
+             'Gender_Churn', 'Education_Level_Churn', 'Marital_Status_Churn', 
+             'Income_Category_Churn', 'Card_Category_Churn']
+    x_features[keep_cols]= data_frame[keep_cols]
+    x_train, x_test, y_train, y_test = train_test_split(x_features, 
+                                                        y_target, test_size= 0.3, random_state=42)
+    return x_train, x_test, y_train, y_test
+
 
 def classification_report_image(y_train,
                                 y_test,
@@ -96,6 +141,7 @@ def feature_importance_plot(model, X_data, output_pth):
     '''
     pass
 
+
 def train_models(X_train, X_test, y_train, y_test):
     '''
     train, store model results: images + scores, and store models
@@ -108,3 +154,24 @@ def train_models(X_train, X_test, y_train, y_test):
               None
     '''
     pass
+
+
+def main():
+    pth = "./data/bank_data.csv"
+    df = import_data(pth)
+    perform_eda(df)
+    cat_columns = [
+    'Gender',
+    'Education_Level',
+    'Marital_Status',
+    'Income_Category',
+    'Card_Category'                
+]
+    df= encoder_helper(df,cat_columns)
+    print(df["Education_Level_Churn"])
+    x_train,x_test,y_train,y_test=perform_feature_engineering(df)
+
+
+
+if __name__ == '__main__':
+    main()
